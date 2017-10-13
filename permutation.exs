833f00afdee86ad4c56dcc0779d6c57697e21505
Dave Lively <davelively14@gmail.com>
2017-10-13 17:31:21 -0400

    Permutations complete

--- permutation.exs
+++ permutation.exs
@@ -1,9 +1,33 @@
 defmodule Permutations do
-  def check(str1, str2) do
+  def equal(str1, str2) do
     a = str1 |> String.graphemes
     b = str2 |> String.graphemes
-    IO.inspect String.graphemes(str1)
+
+    IO.inspect compare(a, b)
+  end
+
+  def compare([], []), do: true
+  def compare([], _), do: false
+  def compare(_, []), do: false
+  def compare([head | tail], series) do
+    if remaining = test_against(head, series) do
+      compare(tail, remaining)
+    else
+      false
+    end
+  end
+
+  def test_against(subj, series), do: test_against(subj, series, [])
+  def test_against(_, [], _), do: nil
+  def test_against(subj, [head | tail], unused) do
+    if subj == head do
+      [unused | tail] |> List.flatten
+    else
+      test_against(subj, tail, [head | unused])
+    end
   end
 end
 
-Permutations.check("john", "nohj")
+Permutations.equal("john", "nohj")
+Permutations.equal("john", "ohj")
+Permutations.equal("john", "ohjjn")
